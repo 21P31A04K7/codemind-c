8cd17f405b7e09ac09d30622482d170cee383fba
21P31A04K7 <99869265+21P31A04K7@users.noreply.github.com>
2024-03-07 10:53:33 +0530

    #include<stdio.h> int main() {     int p,t,r,SI;     scanf("%d%d%d",&p,&t,&r);     SI =(p*t*r)/100;     printf("%d",SI); }

--- SIMPLE_INTEREST.c
+++ SIMPLE_INTEREST.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main()
 {
-   int p,t,r,sp;
-   scanf("%d%d%d",&p,&t,&r);
-   sp=p*t*r/100;
-   printf("%d",sp);
+    int p,t,r,SI;
+    scanf("%d%d%d",&p,&t,&r);
+    SI =(p*t*r)/100;
+    printf("%d",SI);
 }
